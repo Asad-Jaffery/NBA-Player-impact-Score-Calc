b3294dfddface6b7d33e675a0f42d513cdda4408
Asad-Jaffery <asadmjaffery@gmail.com>
2025-03-15 15:56:49 -0700

    chore: add stat weights to config file

---
+++ src/transform/config.py
@@ -0,0 +1,11 @@
+STAT_WEIGHTS = {
+    'MIN': 0.05,
+    'FG_PCT': 0.15,
+    'REB': 0.10,
+    'AST': 0.15,
+    'PTS': 0.20,
+    'STL': 0.05,
+    'BLK': 0.05,
+    'TOV': -0.05,
+    'PLUS_MINUS': 0.25
+}
