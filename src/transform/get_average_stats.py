0e54db616a363e7502debed44bd00eb187682439
Asad-Jaffery <asadmjaffery@gmail.com>
2025-03-16 18:24:01 -0700

    feat: populated player average collections

--- src/transform/get_average_stats.py
+++ src/transform/get_average_stats.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from nba_api.stats.static import teams
+import pandas as pd
 
 
 # connect to mongo
@@ -7,6 +8,10 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["nba_database"]
 
 nba_teams = teams.get_teams()
+
+
+stats_to_count = ['Player_ID','MIN','FG_PCT','REB','AST','PTS','STL','BLK','TOV','PLUS_MINUS']
+
 
 for team in nba_teams:
     team_name = team['full_name']
@@ -16,11 +21,28 @@ for team in nba_teams:
     average_collection = db[collection_name]
     print(f"Created {collection_name}")
 
+    # get the current team's db
     curr_team_collection = db[f"{team_name}_db"]
 
+    # for each player in the current team's db, get the averages and add it 
     for player in curr_team_collection.find({}):
-        print(player["player_name"])
+        print(f"filling {average_collection}...")
 
+        all_game_logs = []
+        all_game_logs.extend(player["game_logs"])
+        all_game_log_df = pd.DataFrame(all_game_logs)
+        if len(all_game_log_df) > 0 :
+            all_game_log_df = all_game_log_df[stats_to_count]
+        else:
+            all_game_log_df = pd.DataFrame()
+        player_averages = pd.DataFrame(all_game_log_df).mean().to_dict()
+        
 
-    # each db has an object for each player 
-    # each player's object contains their averate stats
+        
+        average_collection.insert_one({
+            "player_id": player["player_id"],
+            "player_name": player["player_name"],
+            "team_id": player["team_id"],
+            "team_name": player["team_name"],
+            "avg_stats": player_averages
+            })
