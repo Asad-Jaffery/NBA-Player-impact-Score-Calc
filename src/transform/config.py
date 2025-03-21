b3b81b7359f7e7df0014b1438844089e6107a693
Asad-Jaffery <asadmjaffery@gmail.com>
2025-03-21 13:11:30 -0700

    chore: store call to db in confil file instead

--- src/transform/config.py
+++ src/transform/config.py
@@ -1,3 +1,5 @@
+from pymongo import MongoClient
+
 STAT_WEIGHTS = {
     'MIN': 0.05,
     'FG_PCT': 0.15,
@@ -11,3 +13,10 @@ STAT_WEIGHTS = {
 }
 
 stats_to_count = ['Player_ID','MIN','FG_PCT','REB','AST','PTS','STL','BLK','TOV','PLUS_MINUS']
+
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["nba_database"]
+averages_db = client["nba_averages"]
+
+
