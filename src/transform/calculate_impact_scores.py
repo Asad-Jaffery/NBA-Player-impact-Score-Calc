d754f86e26ca162b47ff555467042489721a1beb
Asad-Jaffery <asadmjaffery@gmail.com>
2025-03-21 15:34:17 -0700

    feat: calculates impact score!!!

--- src/transform/calculate_impact_scores.py
+++ src/transform/calculate_impact_scores.py
@@ -1,11 +1,40 @@
 import pandas as pd
 from pymongo import MongoClient
-from transform.config import STAT_WEIGHTS
+from nba_api.stats.static import teams
+from mongodb_min_max_finder import all_stats_max, all_stats_min
+import config
 
+db = config.db
+averages_db = config.averages_db
+stat_weight = config.STAT_WEIGHTS
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["nba_database"]
+def getPlayerImpactScore(player): # will get the impact score of a player
+    
+    if not player["avg_stats"]:
+        return 0
+    
 
-impact_scores_collection = db["impact_scores"]
+    normalized_stats = {} # normalize the player stats (on a 0 - 100 scale)
+
+    for stat in config.positive_stats:
+        if player["avg_stats"][stat] == 0:
+            normalized_stats[stat] = 0
+        else:
+            normalized_stats[stat] = 100 * (player["avg_stats"][stat] - all_stats_min[stat]) / (all_stats_max[stat] -  all_stats_min[stat])
+
+    normalized_stats['TOV'] = 100 * (all_stats_max['TOV'] - player["avg_stats"]['TOV']) / (all_stats_max['TOV'] - all_stats_min['TOV'])
+
+    if player["avg_stats"]['PLUS_MINUS'] < 0:
+        normalized_stats['PLUS_MINUS'] = 50 - ((player["avg_stats"]['PLUS_MINUS'] - all_stats_min['PLUS_MINUS']) / (all_stats_max['PLUS_MINUS'] - all_stats_min['PLUS_MINUS'] * 50))
+    else:
+        normalized_stats['PLUS_MINUS'] = 50 + ((player["avg_stats"]['PLUS_MINUS'] - all_stats_min['PLUS_MINUS']) / (all_stats_max['PLUS_MINUS'] - all_stats_min['PLUS_MINUS'] * 50))
+
+    score_stats = {}
+
+    for stat in stat_weight:
+        score_stats[stat] = normalized_stats[stat] * stat_weight[stat]
+    
+    return sum(score_stats.values())
+
 
 
