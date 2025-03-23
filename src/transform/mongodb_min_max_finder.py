2a00ea904fbeddab166d9de0d7ea57f1c9473ed0
Asad-Jaffery <asadmjaffery@gmail.com>
2025-03-23 16:00:59 -0700

    chore: fixed fetch from db bug refactor: imports are now packages

--- src/transform/mongodb_min_max_finder.py
+++ src/transform/mongodb_min_max_finder.py
@@ -1,4 +1,4 @@
-import config
+from src.transform import config
 
 db = config.db
 averages_db = config.averages_db
@@ -8,11 +8,10 @@ all_stats_max = {}
 
 # Iterate through all collections
 for collection_name in averages_db.list_collection_names():
-    collection = db[collection_name]
-    
-    # Fetch all documents from the collection
-    for document in collection.find():
+    collection = averages_db[collection_name]
 
+    for document in collection.find({}):
+   
         stats_dict = document.get('avg_stats', {})
         
         # Update min and max for each stat
@@ -26,14 +25,3 @@ for collection_name in averages_db.list_collection_names():
                 # Update min and max
                 all_stats_min[stat_name] = min(all_stats_min[stat_name], stat_value)
                 all_stats_max[stat_name] = max(all_stats_max[stat_name], stat_value)
-
-del all_stats_min["Player_ID"]
-del all_stats_max["Player_ID"]
-
-print("Minimum values for each stat:")
-for stat, value in all_stats_min.items():
-    print(f"{stat}: {value}")
-
-print("\nMaximum values for each stat:")
-for stat, value in all_stats_max.items():
-    print(f"{stat}: {value}")
