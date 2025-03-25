d8e4f11522708f0aae1d49a3c82957f53a0b3c2f
Asad-Jaffery <asadmjaffery@gmail.com>
2025-03-24 23:26:39 -0700

    feat: delete old databases before running new pipelines

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,4 +1,7 @@
 import subprocess
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
 
 def run_script(module_name):
     """Helper function to run a module using `-m` to handle imports properly."""
@@ -9,11 +12,16 @@ def run_script(module_name):
         exit(1)  # Exit if any script fails
 
 if __name__ == "__main__":
+    print("Deleting Old Data First...")
+    client.drop_database("nba_database")
+    client.drop_database("nba_averages")
+    client.drop_database("scores")
+
     print("Starting NBA Impact Score Pipeline...")
 
     print("\n[STEP 1] Extracting raw NBA stats...")
     run_script("src.extract.fetch_nba_data")
-
+    
     print("\n[STEP 2] Fetching averages...")
     run_script("src.transform.get_average_stats")
 
